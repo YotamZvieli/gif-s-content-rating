5d1a74df42dddb5beb41f5348a85ca2d5a0f1ea7
YotamZvieli <116752891+YotamZvieli@users.noreply.github.com>
2024-10-17 13:38:12 +0300

    pre trained model to fine tune.

--- convert-gif-to-avi.py
+++ convert-gif-to-avi.py
@@ -8,16 +8,16 @@ if __name__ == '__main__':
     dataset_root_path = pathlib.Path(dataset_root_path)
     dataset_target_path = "gifs/DL project - gifs - avi"
     dataset_target_path = pathlib.Path(dataset_target_path)
-    video_file_paths_class_g = list(dataset_root_path.glob("train/g/*.gif"))
-    video_file_paths_class_pg = list(dataset_root_path.glob("train/pg/*.gif"))
-    video_file_paths_class_pg13 = list(dataset_root_path.glob("train/pg-13/*.gif"))
-    video_file_paths_class_r = list(dataset_root_path.glob("train/r/*.gif"))
+    video_file_paths_class_g = list(dataset_root_path.glob("test/g/*.gif"))
+    video_file_paths_class_pg = list(dataset_root_path.glob("test/pg/*.gif"))
+    video_file_paths_class_pg13 = list(dataset_root_path.glob("test/pg-13/*.gif"))
+    video_file_paths_class_r = list(dataset_root_path.glob("test/r/*.gif"))
 
-    for gif_path in (video_file_paths_class_g + video_file_paths_class_pg13 + video_file_paths_class_pg):
+    for gif_path in (video_file_paths_class_g + video_file_paths_class_pg13 + video_file_paths_class_pg + video_file_paths_class_r):
         gif = Image.open(gif_path)
 
         split_path = gif_path.parts
-        avi_path = "gifs/DL project - gifs/train_avi/" + split_path[-2] + "/" + split_path[-1] + ".avi"
+        avi_path = "gifs/DL project - gifs/test_avi/" + split_path[-2] + "/" + split_path[-1] + ".avi"
         fourcc = cv2.VideoWriter_fourcc(*'XVID')
 
         try:
--- pre-trained-modle.py
+++ pre-trained-modle.py
@@ -3,6 +3,9 @@ from transformers import VideoMAEImageProcessor, VideoMAEForVideoClassification
 import pytorchvideo.data
 import os
 import imageio
+from transformers import TrainingArguments, Trainer
+import evaluate
+import torch
 import numpy as np
 from IPython.display import Image
 from pytorchvideo.transforms import (
@@ -13,7 +16,6 @@ from pytorchvideo.transforms import (
     ShortSideScale,
     UniformTemporalSubsample,
 )
-
 from torchvision.transforms import (
     Compose,
     Lambda,
@@ -23,6 +25,8 @@ from torchvision.transforms import (
 )
 
 if __name__ == '__main__':
+    print(torch.cuda.is_available())
+    print(torch.cuda.get_device_name(0))
     dataset_root_path = "gifs/DL project - gifs"
     dataset_root_path = pathlib.Path(dataset_root_path)
     video_count_train = len(list(dataset_root_path.glob("train_avi/*/*.gif")))
@@ -66,7 +70,7 @@ if __name__ == '__main__':
                         UniformTemporalSubsample(num_frames_to_sample),
                         Lambda(lambda x: x / 255.0),
                         Normalize(mean, std),
-                        RandomShortSideScale(min_size=256, max_size=320),
+                        RandomShortSideScale(min_size=224, max_size=320),
                         RandomCrop(resize_to),
                         RandomHorizontalFlip(p=0.5),
                     ]
@@ -81,6 +85,62 @@ if __name__ == '__main__':
         decode_audio=False,
         transform=train_transform,
     )
+
+    val_dataset = pytorchvideo.data.Ucf101(
+        data_path=os.path.join(dataset_root_path, "test_avi"),
+        clip_sampler=pytorchvideo.data.make_clip_sampler("random", clip_duration),
+        decode_audio=False,
+        transform=train_transform,
+    )
+
+    model_name = model_ckpt.split("/")[-1]
+    new_model_name = f"{model_name}-finetuned-ucf101-subset"
+    num_epochs = 4
+    batch_size = 2
+
+    args = TrainingArguments(
+        new_model_name,
+        remove_unused_columns=False,
+        eval_strategy="epoch",
+        save_strategy="epoch",
+        learning_rate=5e-5,
+        per_device_train_batch_size=batch_size,
+        per_device_eval_batch_size=batch_size,
+        warmup_ratio=0.1,
+        logging_steps=10,
+        load_best_model_at_end=True,
+        metric_for_best_model="accuracy",
+        max_steps=(train_dataset.num_videos // batch_size) * num_epochs,
+    )
+
+    metric = evaluate.load("accuracy")
+
+
+    def compute_metrics(eval_pred):
+        predictions = np.argmax(eval_pred.predictions, axis=1)
+        return metric.compute(predictions=predictions, references=eval_pred.label_ids)
+
+
+    def collate_fn(examples):
+        # permute to (num_frames, num_channels, height, width)
+        pixel_values = torch.stack(
+            [example["video"].permute(1, 0, 2, 3) for example in examples]
+        )
+        labels = torch.tensor([example["label"] for example in examples])
+        return {"pixel_values": pixel_values, "labels": labels}
+
+
+    trainer = Trainer(
+        model,
+        args,
+        train_dataset=train_dataset,
+        eval_dataset=val_dataset,
+        tokenizer=image_processor,
+        compute_metrics=compute_metrics,
+        data_collator=collate_fn,
+    )
+
+    train_results = trainer.train()
 
 
 
