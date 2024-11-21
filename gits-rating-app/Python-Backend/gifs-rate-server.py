dd5e358131139b5d6da78675ee35869bb6851663
YotamZvieli <116752891+YotamZvieli@users.noreply.github.com>
2024-11-21 20:12:58 +0200

    cleanup

--- gits-rating-app/Python-Backend/gifs-rate-server.py
+++ gits-rating-app/Python-Backend/gifs-rate-server.py
@@ -35,7 +35,6 @@ def upload_file():
 
     file = request.files['gif']
 
-    # If the file is not a valid GIF
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
         file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
